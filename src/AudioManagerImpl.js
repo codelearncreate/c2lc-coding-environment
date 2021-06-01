8362f931a201b9f792fdd8673b9558701a276e6b
Tony Atkins <tony@raisingthefloor.org>
2021-06-01 11:53:09 +0200

    C2LC-395: Expanded 'orchestra'.

--- src/AudioManagerImpl.js
+++ src/AudioManagerImpl.js
@@ -1,7 +1,7 @@
 // @flow
 
 // $FlowFixMe: We need to add a type definition for more stuff.
-import { FMSynth, Panner} from 'tone';
+import {Filter, FMSynth, MembraneSynth, MetalSynth, Panner, Reverb, Synth} from 'tone';
 import CharacterState from './CharacterState';
 import type {IntlShape} from 'react-intl';
 import {AudioManager} from './types';
@@ -43,12 +43,55 @@ export default class AudioManagerImpl implements AudioManager {
             envelope :  {attack:0, decay:0, Sustain:1, Release:1 }
         }).connect(this.panner);
 
-        this.orchestra = {
-            "forward1": marimba,
-            "forward2": marimba,
-            "forward3": marimba
-        };
+        const lowPass = new Filter({
+            frequency: 2000, type:"lowpass"
+        }).connect(this.panner);
 
+        const bellVerb = new Reverb({
+            preDelay:0 , decay:2, wet:0.25
+        }).connect(lowPass);
+
+        const bell = new Synth({
+            oscillator: {
+                type: 'fmsine',
+                modulationType: 'sine',
+                harmonicity: 2,
+                modulationIndex: 8,
+            }
+        }).connect(bellVerb);
+
+        const drum = new MembraneSynth({
+            octaves:4, pitchDecay:0.2
+        }).connect(this.panner);
+
+        const cymbal = new MetalSynth().connect(lowPass);
+
+        // TODO: The NoiseSynth doesn't seem to work for us.  Revisit.
+        // const highPass = new Filter({
+        //     frequency: 9000, type:"highpass"
+        // }).connect(this.panner);
+
+        // const shaker = new FeedbackDelay({
+        //     delayTime:0.1, feedback:0.25
+        // }).connect(highPass);
+
+        // const shake = new NoiseSynth().connect(shaker);
+
+        // TODO: Define sequences instead of using instruments directly for each action.
+        this.orchestra = {
+            "backward1": bell,
+            "backward2": bell,
+            "backward3": bell,
+            "forward1":  marimba,
+            "forward2":  marimba,
+            "forward3":  marimba,
+            "left45":    drum,
+            "left90":    drum,
+            "left180":   drum,
+            "right45":   cymbal,
+            "right90":   cymbal,
+            "right180":  cymbal
+        };
     }
 
     playAnnouncement(messageIdSuffix: string, intl: IntlShape, messagePayload: any) {
