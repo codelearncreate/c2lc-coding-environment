d297afffd03df49398d94f971159edd7cde048f3
Daniel Cho <chosww@gmail.com>
2021-07-30 09:56:54 -0400

    C2LC-431: Add type for expandAddNode keyboard shortcut

--- src/KeyboardInputSchemes.js
+++ src/KeyboardInputSchemes.js
@@ -16,6 +16,7 @@ export type ActionName =
     | "addCommandToBeginning"
     | "addCommandToEnd"
     | "deleteCurrentStep"
+    | "expandAddNode"
     | "announceScene"
     | "decreaseProgramSpeed"
     | "increaseProgramSpeed"
